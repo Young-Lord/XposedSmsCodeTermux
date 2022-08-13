b95bde2000928d344e6b873b30169abbc1ca99d2
LY <51789698+Young-Lord@users.noreply.github.com>
2022-08-13 19:34:18 +0800

    bugfix-;

--- app/src/main/java/com/tianma/xsmscode/xp/hook/code/CodeWorker.java
+++ app/src/main/java/com/tianma/xsmscode/xp/hook/code/CodeWorker.java
@@ -31,7 +31,7 @@ import de.robv.android.xposed.XSharedPreferences;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import android.net.Uri
+import android.net.Uri;
 
 public class CodeWorker {
 
